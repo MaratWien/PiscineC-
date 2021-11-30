22e19354ea5661e25896dce3ae1d93a9ae0485ba
hfunctio <maratwien@gmail.com>
2021-11-30 18:28:19 +0300

    30.11/2

--- CPP Module 00/ex01/AddressBook.cpp
+++ CPP Module 00/ex01/AddressBook.cpp
@@ -1,38 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   AddressBook.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hfunctio <hfunctio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/20 18:10:13 by hfunctio          #+#    #+#             */
-/*   Updated: 2021/11/30 18:25:01 by hfunctio         ###   ########.fr       */
+/*   Created: 2021/11/30 16:03:14 by hfunctio          #+#    #+#             */
+/*   Updated: 2021/11/30 18:23:05 by hfunctio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AddressBook.hpp"
 
-int main(void)
-{
-	std::string command;
-	AddressBook addressBook;
 
-	while (1)
-	{
-		std::cout << "\e[0;37mEnter command: ";
-		std::getline(std::cin, command);
-		if (command == "EXIT")
-			break;
-		else if (command == "ADD")
-		{
-			/* code */
-		}
-		else if (command == "SEARCH")
-		{
-			/* code */
-		}
-		else
-			std::cout << "\e[0;31mError. Forbidden input." << std::endl << "Please enter: ADD, SEARCH, EXIT." << std::endl;
-	}
-	return 0;
-}
---
+++ CPP Module 00/ex01/AddressBook.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   adressBook.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hfunctio <hfunctio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/30 15:53:36 by hfunctio          #+#    #+#             */
+/*   Updated: 2021/11/30 18:22:48 by hfunctio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ADDRESS_BOOK_HPP
+# define ADDRESS_BOOK_HPP
+
+# include <iostream>
+# include <string>
+
+class AddressBook
+{
+
+
+};
+
+#endif
