808a9e15dae877bf87bc3d94978f895f707185a0
hfunctio <maratwien@gmail.com>
2022-01-22 18:17:45 +0300

    22.01

--- CPP Module 01/ex06/main.cpp
+++ CPP Module 01/ex06/main.cpp
@@ -5,22 +5,46 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hfunctio <hfunctio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/22 14:50:09 by hfunctio          #+#    #+#             */
-/*   Updated: 2022/01/22 15:46:01 by hfunctio         ###   ########.fr       */
+/*   Created: 2022/01/22 17:28:23 by hfunctio          #+#    #+#             */
+/*   Updated: 2022/01/22 18:17:25 by hfunctio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Karen.hpp"
 
-int main() {
+int main(int argc< char **argv) {
 
 	Karen tmp;
+	std::string str[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
-	tmp.complain("DEBUG");
-	tmp.complain("INFO");
-	tmp.complain("WARNING");
-	tmp.complain("ERROR");
-	tmp.complain("WARNING");
-	
+	if (argc != 2) {
+		std:cout << "Error. Wrong number of arguments." << '\n';
+		return (0);
+	}
+	for (int i = 0; i < 4; i++) {
+		
+		if (str[i] == argv[1]) {
+
+			std::cout << '[' << argv[1] << ']' << '\n';
+			switch (i) {
+
+				case 0:
+					tmp.complain(str[0]);
+					i++;
+				case 1:
+					tmp.complain(str[1]);
+					i++;
+				case 2:
+					tmp.complain(str[2]);
+					i++;
+				case 3:
+					tmp.complain(str[3]);
+					i++;
+			}
+			return (0);
+		}
+	}
+	std::cout << "[ Probably complaining about insignificant problems ]" << '\n';
+
 	return(0);
 }
